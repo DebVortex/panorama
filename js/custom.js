96cf74797671b607000ae857633ebb2666058ab7
Max Brauer <max.brauer@inqbus.de>
2014-01-12 17:03:45 +0100

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('html').niceScroll();
+
+    $('#widgetcontent_calendar').datepicker({
+        showWeek: true,
+        firstDay: 1});
+
+    $( ".column" ).sortable({
+        connectWith: ".column",
+        cursor: "move",
+        cancel: ".widget-content",
+        delay: 150,
+        forceHelperSize: true,
+        forcePlaceholderSize: true,
+        toleranceType: "pointer",
+        stop: function() { saveOrder(); }
+    });
+
+    $('.widget-content')
+        .on('shown.bs.collapse', function() {
+            saveOrder();
+        })
+        .on('hidden.bs.collapse', function() {
+            saveOrder();
+        });
+
+    $('#restore-widget-order').click(function () {
+        defaultOrder();
+    });
+    restoreOrder();
+});
---
+++ js/dashboard.js
@@ -0,0 +1,41 @@
+var cookieName = "panorama-dashboard";
+
+function saveOrder() {
+    cookieContent = {};
+    $('.widget').each(function (index, value) {
+        var self = $(value);
+        var widgetID = self.attr('id');
+        var column = self.parent().attr('id');
+        var index_in_col = self.index();
+        var is_collapsed = !self.find('.widget-content').hasClass('in');
+        if (!cookieContent[column]) {
+            cookieContent[column] = {};
+        }
+        cookieContent[column][index_in_col] = {
+            'widgetID': widgetID, 'is_collapsed': is_collapsed
+        };
+    });
+    $.cookie(cookieName, JSON.stringify(cookieContent));
+}
+
+function restoreOrder() {
+    var dashboar_order = JSON.parse($.cookie(cookieName));
+    for (var column_id in dashboar_order) {
+        var column = $('#' + column_id);
+        var column_info = dashboar_order[column_id];
+        for (var widget_order in column_info) {
+            var widget_information = column_info[widget_order];
+            var widget = $('#' + widget_information['widgetID']);
+            var is_collapsed = widget_information['is_collapsed'];
+            if (is_collapsed) {
+                widget.find('.widget-content').removeClass('in');
+            }
+            column.append(widget);
+        }
+    }
+}
+
+function defaultOrder() {
+    $.cookie(cookieName, null);
+    location.reload();
+}
